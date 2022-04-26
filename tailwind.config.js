34d2e4c3803b32f8f2651647ba1bbf604e43d906
Jacopo <jacopo.gargiulo@icloud.com>
2022-04-26 19:03:30 +0200

    Add tailwindcss.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss/types').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = config
