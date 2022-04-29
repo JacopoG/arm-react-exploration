3722583c592b7e299fe3fb846795fcf7e0dbd2db
Jacopo <jacopo.gargiulo@icloud.com>
2022-04-29 19:02:21 +0200

    Improve stories code.

--- src/LabFormula.stories.tsx
+++ src/LabFormula.stories.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
 import { LabFormula } from './LabFormula';
 import './index.css';
+import { Story } from '@storybook/react';
 
 export default {
   title: 'LabFormula',
   component: LabFormula
 }
 
-export const Plain = () => <LabFormula int="2" theory="4" aura="8" />;
+export const Plain: Story<any> = (args) => <LabFormula {...args} />;
+Plain.args = {
+  int      : 2,
+  theory   : 4,
+  aura     : 8
+}
 
--- src/Value.stories.tsx
+++ src/Value.stories.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
 import { Value } from './Value';
 import './index.css';
+import { Story } from '@storybook/react';
 
 export default {
   title: 'Value',
   component: Value
 }
 
-export const Plain = () => <Value>8</Value>;
+export const Plain: Story<any> = (args) => <Value>{args.children}</Value>;
+Plain.args = {
+  children: 8
+}
 
-export const WithLabel = () => <Value label="Label">8</Value>;
+export const WithLabel: Story<any> = (args) => <Value label={args.label}>8</Value>;
+WithLabel.args = {
+  children: 8,
+  label: "Label"
+}
