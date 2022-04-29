873ab3dc46dcbb5a7e04939a9b45a9724b498ae2
Jacopo <jacopo.gargiulo@icloud.com>
2022-04-29 19:01:06 +0200

    Minor fix.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LabFormula int="2" theory="4" aura="8" />
+        <LabFormula int={2} theory={4} aura={8} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
