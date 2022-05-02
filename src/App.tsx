1955e267342ff6f643cbaeae6e82df249e5e9506
Nyriu <muninitristano@gmail.com>
2022-05-02 17:03:27 +0200

    File organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { LabFormula } from './LabFormula';
+import { LabFormula } from './components/LabFormula';
+import { AdvancementCard } from './components/Advancement'
 
 import { ArmcharService } from './armchar/armchar.service';
+import { Advancement } from './armchar/classes/advancement';
+
 
 function App() {
-  
+
+  let [advs, setAdvs] = useState(); // how to force type here?
+
+  const getAnswer = async () => {
+    const res:Advancement[] = await ArmcharService.getAdvancements();
+    setAdvs(res as any);
+
+    //ArmcharService.getPregameAdvancements();
+  };
+
   useEffect(() => {
-    ArmcharService.getAdvancements();
+    getAnswer();
   }, []);
 
-  //ArmcharService.getPregameAdvancements();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        {/* <AdvancementCard advs={(advs != undefined)? (advs as Advancement[])[0] : undefined }/>
+        <AdvancementCard advs={(advs != undefined)? advs[0] : undefined }/>
+        */}
+
         <LabFormula int={2} theory={4} aura={8} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
